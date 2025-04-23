97d53d7309a396d37b15292771ca8324aef029cd
Stas <88334645+Estas2021@users.noreply.github.com>
2025-04-23 18:36:34 +0300

    сформировал методы из запросов для вынесения в отдельные классы

--- test_post_v1_account.py
+++ test_post_v1_account.py
@@ -22,7 +22,7 @@ def test_post_v1_account():
         'password': password,
     }
 
-    response = requests.post('http://5.63.153.31:5051/v1/account', json=json_data)
+    response = post_v1_account(json_data)
 
     print('\n')
     print(response.status_code)
@@ -32,15 +32,61 @@ def test_post_v1_account():
 
     # 2 Получение писем из почтового ящика
 
-    params = {
-        'limit': '50',
-    }
-
-    response = requests.get('http://5.63.153.31:5025/api/v2/messages', params=params, verify=False)
+    response = get_api_v2_messages(response)
 
     assert response.status_code == 200, "Письма не были получены"
 
     # 3 Получить активационный токен
+    token = get_activation_token_by_login(login, response)
+
+    assert token is not None, f"Токен для пользователя {login} не был получен"
+
+    # 4 Активировать пользака
+
+    response = put_v1_account_token(token)
+
+    print(response.status_code)
+    print(response.text)
+    assert response.status_code == 200, "Пользователь не был активирован"
+
+    # # Авторизация
+
+    json_data = {
+        'login': login,
+        'password': password,
+        'rememberMe': True,
+    }
+
+    response = post_v1_account_login(json_data)
+    print(response.status_code)
+    print(response.text)
+    assert response.status_code == 200, "Пользователь не смог авторизоваться"
+
+
+def post_v1_account_login(
+        json_data
+        ):
+    response = requests.post('http://5.63.153.31:5051/v1/account/login', json=json_data)
+    return response
+
+
+def put_v1_account_token(
+        token
+        ):
+    headers = {
+        'accept': 'text/plain',
+    }
+    response = requests.put(
+        f'http://5.63.153.31:5051/v1/account/{token}',
+        headers=headers
+    )
+    return response
+
+
+def get_activation_token_by_login(
+        login,
+        response
+        ):
     token = None
     try:
         for item in response.json()['items']:
@@ -54,46 +100,21 @@ def test_post_v1_account():
                 print(token)
     except JSONDecodeError:
         print("Response is not a json format")
+    return token
 
-    assert token is not None, f"Токен для пользователя {login} не был получен"
 
-    # 4 Активировать пользака
-
-    url = f'http://5.63.153.31:5051/v1/account/{token}'
-
-    headers = {
-        'Accept': 'application/json, text/plain, */*',
-        'Accept-Language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7',
-        'Cache-Control': 'no-cache',
-        'Connection': 'keep-alive',
-        'Pragma': 'no-cache',
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.0.0 Safari/537.36',
+def get_api_v2_messages(
+        response
+        ):
+    params = {
+        'limit': '50',
     }
+    response = requests.get('http://5.63.153.31:5025/api/v2/messages', params=params, verify=False)
+    return response
 
-    response = requests.put(
-        url,
-        headers=headers
-    )
-    print(response.status_code)
-    print(response.text)
-    # # Пытаемся парсить JSON только если ответ не пустой
-    # try:
-    #     response_json = response.json()
-    #     print("JSON Response:", response_json)
-    #     pprint.pprint(response_json)
-    # except requests.exceptions.JSONDecodeError:
-    #     print("Ошибка: Сервер вернул невалидный JSON или пустой ответ.")
-    assert response.status_code == 200, "Пользователь не был активирован"
 
-    # # Авторизация
-
-    json_data = {
-        'login': login,
-        'password': password,
-        'rememberMe': True,
-    }
-
-    response = requests.post('http://5.63.153.31:5051/v1/account/login', json=json_data)
-    print(response.status_code)
-    print(response.text)
-    assert response.status_code == 200, "Пользователь не смог авторизоваться"
+def post_v1_account(
+        json_data
+        ):
+    response = requests.post('http://5.63.153.31:5051/v1/account', json=json_data)
+    return response
