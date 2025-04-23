da8ea8ec2ea90cf50743e002793bbfc2cbdf78af
Stas <88334645+Estas2021@users.noreply.github.com>
2025-04-23 19:33:47 +0300

    вынесли функции в классы клиенты

--- tests/functional/post_v1_account/test_post_v1_account.py
+++ tests/functional/post_v1_account/test_post_v1_account.py
@@ -1,18 +1,21 @@
-import requests
-import pprint
-
 from json import (
     loads,
-    JSONDecodeError
+    JSONDecodeError,
 )
+
+from dm_api_account.apis.account_api import AccountApi
+from dm_api_account.apis.login_api import LoginApi
+from dm_api_mailhog.apis.mailhog_api import MailhogApi
 
 
 def test_post_v1_account():
     # 1 регистрация пользака
 
-    url = 'http://5.63.153.31:5051/v1/account'
+    account_api = AccountApi(host='http://5.63.153.31:5051/v1/account')
+    login_api = LoginApi(host='http://5.63.153.31:5051/v1/account')
+    mailhog_api = MailhogApi(host='http://5.63.153.31:5025')
 
-    login = 'efremov_test6'
+    login = 'efremov_test9'
     password = '123456789'
     email = f'{login}@mail.ru'
 
@@ -22,7 +25,7 @@ def test_post_v1_account():
         'password': password,
     }
 
-    response = post_v1_account(json_data)
+    response = account_api.post_v1_account(json_data=json_data)
 
     print('\n')
     print(response.status_code)
@@ -32,7 +35,7 @@ def test_post_v1_account():
 
     # 2 Получение писем из почтового ящика
 
-    response = get_api_v2_messages(response)
+    response = mailhog_api.get_api_v2_messages()
 
     assert response.status_code == 200, "Письма не были получены"
 
@@ -43,7 +46,7 @@ def test_post_v1_account():
 
     # 4 Активировать пользака
 
-    response = put_v1_account_token(token)
+    response = account_api.put_v1_account_token(token=token)
 
     print(response.status_code)
     print(response.text)
@@ -57,36 +60,16 @@ def test_post_v1_account():
         'rememberMe': True,
     }
 
-    response = post_v1_account_login(json_data)
+    response = login_api.post_v1_account_login(json_data=json_data)
     print(response.status_code)
     print(response.text)
     assert response.status_code == 200, "Пользователь не смог авторизоваться"
 
 
-def post_v1_account_login(
-        json_data
-        ):
-    response = requests.post('http://5.63.153.31:5051/v1/account/login', json=json_data)
-    return response
-
-
-def put_v1_account_token(
-        token
-        ):
-    headers = {
-        'accept': 'text/plain',
-    }
-    response = requests.put(
-        f'http://5.63.153.31:5051/v1/account/{token}',
-        headers=headers
-    )
-    return response
-
-
 def get_activation_token_by_login(
         login,
         response
-        ):
+):
     token = None
     try:
         for item in response.json()['items']:
@@ -101,20 +84,3 @@ def get_activation_token_by_login(
     except JSONDecodeError:
         print("Response is not a json format")
     return token
-
-
-def get_api_v2_messages(
-        response
-        ):
-    params = {
-        'limit': '50',
-    }
-    response = requests.get('http://5.63.153.31:5025/api/v2/messages', params=params, verify=False)
-    return response
-
-
-def post_v1_account(
-        json_data
-        ):
-    response = requests.post('http://5.63.153.31:5051/v1/account', json=json_data)
-    return response
