7aff8410ec39de8722a91be68265fe7c726a8016
Stas <88334645+Estas2021@users.noreply.github.com>
2025-04-24 19:19:40 +0300

    создали фасады. Сперва отдельные. Затем мегафасад.

--- tests/functional/post_v1_account/test_post_v1_account.py
+++ tests/functional/post_v1_account/test_post_v1_account.py
@@ -1,10 +1,13 @@
+import time
 from json import (
     loads,
     JSONDecodeError,
 )
-
 import structlog
 
+from helpers.account_helper import AccountHelper
+from restclient.configuration import Configuration as DmApiConfiguration
+from restclient.configuration import Configuration as MailhogConfiguration
 
 structlog.configure(
     processors=[
@@ -16,77 +19,29 @@ structlog.configure(
     ]
 )
 
-from dm_api_account.apis.account_api import AccountApi
-from dm_api_account.apis.login_api import LoginApi
-from dm_api_mailhog.apis.mailhog_api import MailhogApi
-
+from services.api_mailhog import MailHogApi
+from services.dm_api_account import DMApiAccount
 
 def test_post_v1_account():
-    # 1 регистрация пользака
+# 1 регистрация пользака
 
-    account_api = AccountApi(host='http://5.63.153.31:5051')
-    login_api = LoginApi(host='http://5.63.153.31:5051')
-    mailhog_api = MailhogApi(host='http://5.63.153.31:5025')
+    mailhog_configuration = MailhogConfiguration(host='http://5.63.153.31:5025')
+    dm_api_configuration = DmApiConfiguration(host='http://5.63.153.31:5051', disable_log=False)
 
-    login = 'efremov_test23'
+    account = DMApiAccount(configuration=dm_api_configuration)
+    mailhog = MailHogApi(configuration=mailhog_configuration)
+
+    account_helper = AccountHelper(dm_account_api=account, mailhog=mailhog) # мегаФАСАД
+
+    login = 'efremov_test40'
     password = '123456789'
     email = f'{login}@mail.ru'
 
-    json_data = {
-        'login': login,
-        'email': email,
-        'password': password,
-    }
+    account_helper.register_new_user(login=login,password=password,email=email)
 
-    response = account_api.post_v1_account(json_data=json_data)
-
-    assert response.status_code == 201, f"Пользак не был создан {response.json()}"
-
-    # 2 Получение писем из почтового ящика
-
-    response = mailhog_api.get_api_v2_messages()
-
-    assert response.status_code == 200, "Письма не были получены"
-
-    # 3 Получить активационный токен
-    token = get_activation_token_by_login(login, response)
-
-    assert token is not None, f"Токен для пользователя {login} не был получен"
-
-    # 4 Активировать пользака
-
-    response = account_api.put_v1_account_token(token=token)
-
-    assert response.status_code == 200, "Пользователь не был активирован"
-
-    # # Авторизация
-
-    json_data = {
-        'login': login,
-        'password': password,
-        'rememberMe': True,
-    }
-
-    response = login_api.post_v1_account_login(json_data=json_data)
-
-    assert response.status_code == 200, "Пользователь не смог авторизоваться"
+    account_helper.login_user(login=login, password=password)
 
 
-def get_activation_token_by_login(
-        login,
-        response
-):
-    token = None
-    try:
-        for item in response.json()['items']:
-            user_data = loads(item['Content']['Body'])
-            user_login = user_data['Login']
 
-            if user_login == login:
-                # print('\n')
-                # print(user_login)
-                token = user_data['ConfirmationLinkUrl'].split('/')[-1]
-                # print(token)
-    except JSONDecodeError:
-        # print("Response is not a json format")
-        return token
+
+
